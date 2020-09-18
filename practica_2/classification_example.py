448b18ba70784c41055e65f5887e5ec723c8de2b
Guillermo Herrera Acosta <a01400835@itesm.mx>
2020-09-17 21:55:05 -0500

    Update practica3
    Signed-off-by: Guillermo Herrera Acosta <a01400835@itesm.mx>

--- practica_2/classification_example.py
+++ practica_2/classification_example.py
@@ -1,39 +1,41 @@
-"""
-Problema 4
-Calcula la exactitud de un clasificador k-NN
-con k-fold cross validation (5 pliegues)
-para k = 1, 2, 3, 4, ..., 10 del clasificador.
-"""
-import matplotlib.pyplot as plt
+#------------------------------------------------------------------------------------------------------------------
 import numpy as np
 
 from glob import glob
+from sklearn import datasets
 from sklearn import svm
-from random import random
 from sklearn.model_selection import KFold
 from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 
 
-
-# Import datos misteriosos
+# Import Datos misteriosos data set
 files = glob("*.txt")
 data = np.loadtxt(files[0])
 
-# Train KNN classifier with all the available observations
-sample_size = 3*len(data)//4
-x = data[sample_size:]
-y = data[:sample_size]
+x = data.data
+y = data.target
+features = data.feature_names
+n_features = len(features)
+
 clf = KNeighborsClassifier(n_neighbors=5)
 clf.fit(x,y)
 
+# Train SVM classifier with all the available observations
+clf = svm.SVC(kernel = 'linear')
+clf.fit(x, y)
+
 # Predict one new sample
-# print("Prediction for a new observation", clf.predict( [[random() * 2 for _ in range(len(data[0]))]] ))
+print("Prediction for a new observation", clf.predict( [[1.,2.,3.,4.]] ))
 
 # 5-fold cross-validation
 kf = KFold(n_splits=5, shuffle = True)
+clf = svm.SVC(kernel = 'linear')
+
 acc = 0
 for train_index, test_index in kf.split(x):
+
     # Training phase
     x_train = x[train_index, :]
     y_train = y[train_index]
@@ -46,7 +48,7 @@ for train_index, test_index in kf.split(x):
 
     # Calculate confusion matrix and model performance
     cm = confusion_matrix(y_test, y_pred)
-    acc_i = (sum(cm[i,i] for i in range(len(cm))))/len(y_test)    
+    acc_i = (cm[0,0]+cm[1,1]+cm[2,2])/len(y_test)    
     print('acc = ', acc_i)
 
     acc += acc_i 
--- practica_3/mlp_example.py
+++ practica_3/mlp_example.py
@@ -1,43 +1,42 @@
-#------------------------------------------------------------------------------------------------------------------
 import numpy as np
-
-from glob import glob
 from sklearn import datasets
 from sklearn.model_selection import KFold
 from sklearn.metrics import confusion_matrix
-
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import np_utils
+from sklearn import datasets
+from sklearn import svm
 
 # Import Datos misteriosos data set
-files = glob("../practica_2/*.txt")
-with open(files[0], "r") as f:
-    data = [
-        register.split()[1:]
-        for register
-        in f.readlines()
-    ]
+datos = np.loadtxt("datos.txt")
+x = datos[:, 1:]
+y = datos[:, 0]
+y = y -1
+n_features = x.shape[1]
 
+targets = ["Clase1", "Clase2"]
+n_clases = len(targets)
+
+features = [str(i) for i in range(len(x[0]))]
+n_features = len(x[0])
 
 # Create output variables from original labels
-output_y = np_utils.to_categorical(y)   # This is only required in 
-print(output_y)
+output_y = np_utils.to_categorical(y)   # This is only required in
 
 # Define MLP model
 clf = Sequential()
 clf.add(Dense(8, input_dim=n_features, activation='relu'))
 clf.add(Dense(8, activation='relu'))
-clf.add(Dense(3, activation='softmax')) # for 2-class problems, use clf.add(Dense(1, activation='sigmoid'))
-
+#clf.add(Dense(3, activation='softmax')) # for 2-class problems, use clf.add(Dense(1, activation='sigmoid'))
+clf.add(Dense(1, activation='sigmoid'))
 # Compile model
 clf.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
- 
 # Fit model
-clf.fit(x, output_y, epochs=150, batch_size=5)
+clf.fit(x, output_y, epochs=150, batch_size=8)
 
 # Predict class of a new observation
-prob = clf.predict( [[1.,2.,3.,4.]] )
+prob = clf.predict( datos[:, 1:] )
 print("Probablities", prob)
 print("Predicted class", np.argmax(prob, axis=-1))
 
@@ -45,9 +44,8 @@ print("Predicted class", np.argmax(prob, axis=-1))
 kf = KFold(n_splits=5, shuffle = True)
 
 acc = 0
-recall = np.array([0., 0., 0.])
+recall = np.array([0., 0.])
 for train_index, test_index in kf.split(x):
-
     # Training phase
     x_train = x[train_index, :]
     y_train = y[train_index]
@@ -57,25 +55,54 @@ for train_index, test_index in kf.split(x):
     clf.add(Dense(8, input_dim=n_features, activation='relu'))
     clf.add(Dense(8, activation='relu'))
     clf.add(Dense(3, activation='softmax'))
-    clf.compile(loss='categorical_crossentropy', optimizer='adam') # For 2-class problems, use clf.compile(loss='binary_crossentropy', optimizer='adam')
+    #clf.compile(loss='categorical_crossentropy', optimizer='adam') # For 2-class problems, use clf.compile(loss='binary_crossentropy', optimizer='adam')
+    clf.compile(loss='binary_crossentropy', optimizer='adam')
     clf.fit(x_train, y_train, epochs=150, batch_size=5, verbose=0)    
 
     # Test phase
     x_test = x[test_index, :]
     y_test = y[test_index]
-    y_pred = np.argmax(clf.predict(x_test), axis=-1)  # For 2-class problems, use (clf.predict(x_test) > 0.5).astype("int32")
+    #y_pred = np.argmax(clf.predict(x_test), axis=-1)  # For 2-class problems, use (clf.predict(x_test) > 0.5).astype("int32")
+    y_pred = np.argmax((clf.predict(x_test) > 0.5).astype("int32"))
 
     cm = confusion_matrix(y_test, y_pred)
+    acc += (cm[0,0]+cm[1,1])/len(y_test)    
 
-    acc += (cm[0,0]+cm[1,1]+cm[2,2])/len(y_test)    
-
-    recall[0] += cm[0,0]/(cm[0,0] + cm[0,1] + cm[0,2])
-    recall[1] += cm[1,1]/(cm[1,0] + cm[1,1] + cm[1,2])
-    recall[2] += cm[2,2]/(cm[2,0] + cm[2,1] + cm[2,2])
-
+    recall[0] += cm[0,0]/(cm[0,0] + cm[0,1])
+    recall[1] += cm[1,1]/(cm[1,0] + cm[1,1])
 
 acc = acc/5
 print('ACC = ', acc)
 
 recall = recall/5
 print('RECALL = ', recall)
+
+# Train SVM classifier with all the available observations 
+clf = svm.SVC(kernel = 'linear') 
+clf.fit(x, y) 
+ 
+# Predict one new sample 
+print("Prediction for a new observation", clf.predict(datos[:, 1:]) ) 
+ 
+# 5-fold cross-validation 
+kf = KFold(n_splits=5, shuffle = True) 
+clf = svm.SVC(kernel = 'linear') 
+ 
+acc = 0 
+for train_index, test_index in kf.split(x): 
+    # Training phase 
+    x_train = x[train_index, :] 
+    y_train = y[train_index] 
+    clf.fit(x_train, y_train) 
+    # Test phase 
+    x_test = x[test_index, :] 
+    y_test = y[test_index]     
+    y_pred = clf.predict(x_test) 
+    # Calculate confusion matrix and model performance 
+    cm = confusion_matrix(y_test, y_pred) 
+    acc_i = (cm[0,0]+cm[1,1]+cm[2,2])/len(y_test)     
+    print('acc = ', acc_i) 
+    acc += acc_i  
+ 
+acc = acc/5 
+print('ACC = ', acc)
